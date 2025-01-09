89561b16460bfe6328e70365e84ba5445e7b5d07
John C. Thomas <jcthomas000@gmail.com>
2025-01-09 14:27:00 +0000

    tests for writing png, and dealing with paired/single-end

--- src/jtmethtools/tests/tests.py
+++ src/jtmethtools/tests/tests.py
@@ -22,8 +22,9 @@ logger.remove()
 
 TESTDIR = Path(os.path.dirname(__file__))
 
-samstr = """@HD	VN:1.0	SO:coordinate
+samstr = """@HD	VN:1.0	SO:none
 @SQ	SN:1	LN:248956422
+mapq20Unorder	18	1	100	20	10M	*	0	10	TTTTTTTTTT	JJJJJJJJJJ	NM:i:tag0\tMD:Z:tag1\tXM:Z:hhhhhhhhhh
 unmethRev	18	1	100	42	10M	*	0	10	TTTTTTTTTT	JJJJJJJJJJ	NM:i:tag0\tMD:Z:tag1\tXM:Z:hhhhhhhhhh
 methRev	18	1	101	32	10M	*	0	10	CCCCCCCCCC	JJJJJJJJJJ	NM:i:tag0\tMD:Z:tag1\tXM:Z:HHHHHHHHHH
 unmethFor	2	1	102	27	10M	*	0	10	TTTTTTTTTT	ABCDEFGHIJ	NM:i:tag0\tMD:Z:tag1\tXM:Z:zzzzzzzzzz
@@ -38,20 +39,46 @@ mapq19	18	1	100	19	10M	*	0	10	TTTTTTTTTT	JJJJJJJJJJ	NM:i:tag0\tMD:Z:tag1\tXM:Z:h
 mapq20	18	1	100	20	10M	*	0	10	TTTTTTTTTT	JJJJJJJJJJ	NM:i:tag0\tMD:Z:tag1\tXM:Z:hhhhhhhhhh
 """
 
-regionsstr = "1\t90\t110\ttest1\n"
-test_bam_fn = TESTDIR/'img-test.sam'
-with open(test_bam_fn, 'w') as f:
+fn_test_bam = TESTDIR / 'img-test.sam'
+with open(fn_test_bam, 'w') as f:
     f.write(samstr)
 
-    test_regions_fn = TESTDIR/'regions.bed'
-    with open(test_regions_fn, 'w') as f:
-        f.write(regionsstr)
+fn_test_regions = TESTDIR/'regions.tsv'
+with open(fn_test_regions, 'w') as f:
+    f.write("Name	Chrm	Start	End	Threshold	Direction\n")
+    f.write("test1\t1\t90\t110\t0\n")
 
 read_data = alignment_data.process_bam(
-    test_bam_fn,
-    test_regions_fn,
-    include_read_name=True
+    fn_test_bam,
+    fn_test_regions,
+    include_read_name=True,
+    single_ended=True,
 )
+
+def test_process_paired():
+    samsorted = """@HD	VN:1.0	SO:queryname	SS:queryname:natural
+@SQ	SN:1	LN:248956422
+K00252:520:HCKCTBBXY:5:1101:1367:45396_1:N:0:ANCGATAG+GCCTCTAT	83	1	1113568	23	96M	=	1113307	-357	ACATATTAACAACAATAAACCTCAATTTTTCCCTCCTCAAAAAAATTTAATAAACGAATAAAAAACAAAAAAAACCTTAAAACAAAAACAACAATT	<A-AF77777--A7AFA--AJFAFF<AA<A<AFFFA-AJFFJJJJJJJJJJFFF7JJJFFA<JFJJJ-JJA-<JJJJFF<--JJJF<AFFJJFAJJ	NM:i:27	MD:Z:0G3G3G2G2G1G22G2G1G3G2G0G3G0G2G0G1G0G2G2G1G6G1G2G0G1G5G2	XM:Z:h...h...h..x..x.h......................x..h.h...h..hh..Zxh..hh.hh..x..h.h......h.h..xh.h.....x..	XR:Z:CT	XG:Z:GA
+K00252:520:HCKCTBBXY:5:1101:1367:45396_1:N:0:ANCGATAG+GCCTCTAT	163	1	1113307	23	117M	=	1113568	357	TAAAACTCAAATACATTCTACCAATAATAAAACTCTAAAAACTCACCTAAACTCCACACTCCCCCTCGACACACGCAACCCTCAACCAACATAATAAAAAAAAATCACTCTTTCTTT	JF-F<FJJ7-AJ<FFJJJFAF<JAA-AFFFAFFF-A7<A--<FJFJFAAF<AAFFFFFJFF--7A<A<F<7JFFFJ--)AAFA)-AJAA--<<JAF<-7<-A-FA---7A777F7--	NM:i:26	MD:Z:1G7G9G3G5G1G4G2G0G7G1G11A5G15G3G0A0G1G0G1G1G2G1G0G1G0G10	XM:Z:.x.......x.........x...x.....h.h....x..hh.......x.h................Zx.....Z.........x...x.h.hh.h.h..h.hh..h..........	XR:Z:GA	XG:Z:GA
+K00252:520:HCKCTBBXY:5:1101:1387:43884_1:N:0:ANCGATAG+GCCTCTAT	83	1	1034354	42	120M	=	1034332	-142	CCTCCTCCCACAATAAACCTAAAAAACTAAAACCAAAACCTAACACCCCCAAAACTATAACTAAATAAACTCTAAAAACACAACTACTATCCACCGCCCCAAAAATCCCAAAATAAAACG	JF7FFJJJJAFJAFAA7JJJJJJFJAJJFF-FJFAA<7AFJFAF7JJJFFF<JJJJJJA-FJJFFJ<F7JJJJ<JJAFFFFF7JJFJJ<JJJAJJJJJJJAJJJJJJJJJJJJJJJFAJJ	NM:i:42	MD:Z:11G2G1G3G0G1G0G3G0G1G2G0G0G4G0G8G0G0G2G1G3G0G0G1G6G1G0G0G4G2G2G3G8G0G0G0G5G0G2G0G0G0G2	XM:Z:...........z..h.h...xh.hh...xh.h..zxh....xh........xhh..x.h...xhh.h......x.hhh....x..x..x...z..Z.....xhhh.....xh..hhhh.Z	XR:Z:CT	XG:Z:GA
+K00252:520:HCKCTBBXY:5:1101:1387:43884_1:N:0:ANCGATAG+GCCTCTAT	163	1	1034332	42	117M	=	1034354	142	TTCTACTCCAAAAAAATTCTACCCTCCTCCCACAATAAACCTAAAAAACTAAAACCAAAACCTAACACCCCCAAAACTATAACTAAATAAACTCTAAAAACACAACTACTATCCACC	JJJJJJJJJJFFFJJJJFJJJJJJJJFJJJJJJJAJJF-AJF7AJFJJAJF<<FFJJJFFFJFJFJFFJJJJJAFAFFAAFAJJJJ7AJJFA7FFJJJJJF<FF<A<<FAF<AJFFJ	NM:i:37	MD:Z:9G0G0G1G6G12G2G1G3G0G1G0G3G0G1G2G0G0G4G0G8G0G0G2G1G3G0G0G1G6G1G0G0G4G2G2G3G2	XM:Z:.........zxh.h......x............z..h.h...xh.hh...xh.h..zxh....xh........xhh..x.h...xhh.h......x.hhh....x..x..x...z..	XR:Z:GA	XG:Z:GA
+K00252:520:HCKCTBBXY:5:1101:2361:48245_1:N:0:AGCGATAG+GCCTCTAT	99	1	1417392	40	96M	=	1417551	275	GTTTTTGTTGAAAATTTTGGAAGTTTGGGATTTTAATTATTTTTTGTTATTAATAAGATTTTGTTTAGTTTAAATTAATTTAGAATTGGTATAGAT	FA-<JJJAJAJJJJF-<-<JJJJ<AJFJJJFFJJFJFAJJ-FJ<FJJJJJJJJJFJJJJF-JAAFJFFAJFFJJJJFJJJJJJJ<AAAJ<JJJJFF	NM:i:11	MD:Z:1C12C0C17C6C0C4C11C0C9C8C17	XM:Z:.h............hh.................h......hh....h...........hh.........h........h.................	XR:Z:CT	XG:Z:CT
+K00252:520:HCKCTBBXY:5:1101:2361:48245_1:N:0:AGCGATAG+GCCTCTAT	147	1	1417551	40	116M	=	1417392	-275	TAATGATCGTAGGGGGGAGGGGGTTTAAAATTTGTTTTAAGTGTTTATGTACGGAAATTGGTTTGGGTGTTTTGGTTTATACGTTATTTTGTGTTATATTTTTGAAATAAGGGATT	-7-<-A-7A7)<-)-<--F7--FAJJAA<AFJJJJFF<---<7--------7------A-<AAF7---JFFJ77F<F7-F--F<A-JFFF<F<A--F-FJJJJF<<A<AAA-A-JA	NM:i:26	MD:Z:3C0T1C17C5C0C2C1C3C3C0C11C3C0C6C0C4C3C6C0C6C3C0C7C6C0C0	XM:Z:...h..xZ................h.....hx..h.h.......hh.....Z.....x...hx......hh....h...h.Z....hh......h...hh.......h......hx	XR:Z:GA	XG:Z:CT
+"""
+
+    fn_sorted = TESTDIR / 'paired-test.sam'
+    with open(fn_sorted, 'w') as f:
+        f.write(samsorted)
+    from jtmethtools.alignments import iter_bam
+    i = None
+    for i, aln in enumerate(iter_bam(fn_sorted, paired_end=True)):
+        pass
+    assert i == 2
+    for i, aln in enumerate(iter_bam(fn_sorted, paired_end=False)):
+        pass
+    assert i == 5
+
+
 
 
 def test_read_write_array():
@@ -78,13 +105,32 @@ def test_read_write_array():
         assert np.all(arr == arr2_read2)
 
 
-# def test_save_images():
-#     start, end = 90, 110
-#     window = read_data.window(start=start, end=end, chrm='1')
-#     img = jtmethtools.images.ImageMaker(window, start, end, rows=20)
-#
-#     fig, axes = img._plot_test_images()
-#     plt.savefig(TESTDIR/'channels.png', dpi=150, )
+def test_save_images():
+    start, end = 90, 110
+    window = read_data.window(start=start, end=end, chrm='1')
+    img = jtmethtools.images.ImageMaker(window, start, end, rows=20)
+
+    from jtmethtools.images import (
+        generate_images_in_regions,
+
+        plot_layer
+    )
+
+    layer_methods = img.available_layers()
+    n = len(layer_methods)
+    fig, axes = plt.subplots(1, n, figsize=(4*n, 4))
+
+    for i, (k, layer) in enumerate(
+            img.get_pixarray_dict(layer_methods).items()
+    ):
+        ax = axes[i]
+        plot_layer(layer, ax=ax)
+        ax.set_xlabel(k)
+
+    plt.savefig(TESTDIR / f'image_layers_test_bam.png')
+
+    # fig, axes = img._plot_test_images()
+    # plt.savefig(TESTDIR/'channels.png', dpi=150, )
 
 
 def check_filtering(rd, rid_in, rid_not_in):
@@ -108,6 +154,8 @@ def test_filter_read_data():
 
     filt_meth = read_data.filter_by_noncpg_met(max_noncpg=0)
     check_filtering(filt_meth, 'unmethRev', 'methRev')
+
+
 
 
 
