a6c5a25f36328ca5b847f7bd4cbf465e946e71d9
John C. Thomas <jcthomas000@gmail.com>
2025-03-04 17:40:51 +0000

    switched to outputting arrays.

--- src/jtmethtools/scripts/read_stats.py
+++ src/jtmethtools/scripts/read_stats.py
@@ -1,67 +1,128 @@
-import matplotlib.pyplot as plt
-import pysam
-from collections import Counter
-from jtmethtools.alignments import get_bismark_met_str
-import  os
-import pandas as pd
+from typing import Tuple, Iterator
 from pathlib import Path
-import datargs
+import  os
+from collections import Counter
 from dataclasses import field
 
-def get_table(bamfn) -> pd.DataFrame:
-    """Generate table of per-read methylation stats from a Bismark BAM."""
-    table = []
+import matplotlib.pyplot as plt
+import pysam
+import pandas as pd
+import numpy as np
+import numpy.typing as npt
+import datargs
+
+from jtmethtools.alignments import get_bismark_met_str
+from jtmethtools.util import write_array, read_array
+
+
+COLUMNS = ( 'Chrm', 'Start', 'Length', 'H', 'X', 'U', 'Z', 'h', 'x', 'u', 'z', 'FreqC', 'Met', 'NotC')
+COLI = {c:i for i, c in enumerate(COLUMNS)}
+
+CHRM_MAP:dict[str, int] = {}
+for i, c in enumerate(list(range(1, 24))+['X', 'Y']):
+    CHRM_MAP[str(c)] = i
+    CHRM_MAP[f"chr{c}"] = i
+
+VALID_CHRM = set(list(CHRM_MAP.keys()))
+
+
+def iter_alignments(bamfn, only_cannon_chrm) -> Iterator[pysam.AlignedSegment]:
     for i, a in enumerate(pysam.AlignmentFile(bamfn)):
+        if not a.is_mapped:
+            continue
+        chrm = a.reference_name
+        if only_cannon_chrm:
+            if chrm not in VALID_CHRM:
+                continue
+            yield a
+        else:
+            try:
+                chrm_i = CHRM_MAP[chrm]
+            except KeyError:
+                chrm_i = max(CHRM_MAP.values())+1
+                CHRM_MAP[chrm] = chrm_i
+            yield a
+
+
+def get_table(bamfn, only_cannonical_chrm=False) -> npt.NDArray[np.uint32]:
+    """Generate table of per-read methylation stats from a Bismark BAM."""
+    i = 0
+    for i, _ in enumerate(iter_alignments(bamfn, only_cannonical_chrm)):
+        pass
+    total_alignments = i+1
+
+    # create arrays to hold the data
+    table = np.zeros((total_alignments, len(COLUMNS)), dtype=np.uint32)
+
+    for i, a in enumerate(iter_alignments(bamfn, only_cannonical_chrm)):
         ln = a.query_length
 
         metstr = get_bismark_met_str(a)
 
-        row = {'AlignmentIndex' :i, 'Chrm' :a.reference_name, 'Start' :a.query_alignment_start, 'Length' :ln}
+        row = np.zeros(len(COLUMNS), dtype=np.uint32)
+        row[COLI['Chrm']] = CHRM_MAP[a.reference_name]
+        row[COLI['Start']] = a.query_alignment_start
+        row[COLI['Length']] = ln
 
         # count glyphs in the methylation string.
         c = dict(Counter(metstr))
-        # change "." to "H"
+        # Give '.' a better name
         c['NotC'] = c['.']
         del c['.']
-        row = row | c
+        for k, v in c.items():
+            row[COLI[k]] = v
 
-        table.append(row)
-    table = pd.DataFrame(table)
-    table = table.fillna(0).astype(int, errors='ignore', copy=False)
+        table[i] = row
 
-    table.loc[:, 'FreqC'] = table.reindex(columns=['H', 'X', 'U', 'Z', 'h', 'x', 'u', 'z' ,]).sum(1).astype(int)
-    table.loc[:, 'Met'] = table.reindex(columns=['H', 'X', 'Z', 'U']).sum(1).astype(int)
+    c_cols = [COLI[k] for k in 'H X U Z h x u z'.split()]
+    met_cols = [COLI[k] for k in 'H X Z U'.split()]
+    table[:, COLI['FreqC']] = table[:, c_cols].sum(1)
+    table[:, COLI['Met']] = table[:, met_cols].sum(1)
 
     return table
 
 
-def plot_len_pmet(table) -> None:
+def plot_len_pmet(table:npt.NDArray) -> None:
     """Plot read length vs methylation status."""
-    m = table.FreqC > 0
-    x = table.loc[m, 'Length']
-    y = table.loc[m, 'Met'] / table.loc[m, 'FreqC']
+    m = table[:, COLI['FreqC']] > 0
+    x = table[m, COLI['Length']]
+    y = table[m, COLI['Met']] / table[m, COLI['FreqC']]
+
     plt.figure(figsize=(4 ,4))
     plt.hexbin(x, y, bins='log', gridsize=50)
     plt.xlabel('Read Length (BP)')
     plt.ylabel('Methylation %')
+    plt.colorbar(label='log10(count)')
+    plt.title('Read Length vs Methylation %')
 
 
-def run(bamfn, out_prefix, do_plot, write_table):
+
+def run(bamfn, out_prefix, do_plot, write_table, cannon_chrm):
 
     out_prefix = Path(out_prefix)
     if not os.path.isdir(out_prefix.parent):
         os.makedirs(out_prefix.parent)
 
-    table = get_table(bamfn)
+    table = get_table(bamfn, cannon_chrm)
 
     if do_plot:
         plot_len_pmet(table)
-        plt.savefig(str(out_prefix)+'.len-vs-met.png')
+
+        plt.savefig(
+            str(out_prefix)+'.len-vs-met.png',
+            dpi=300,
+            bbox_inches='tight'
+        )
+
     if write_table:
-        table.to_csv(
-            str(out_prefix)+'.read-stats.tsv.gz',
-            sep='\t',
-            index=False,
+        write_array(
+            table,
+            str(out_prefix)+'.read-stats.arr.gz',
+            additional_metadata={
+                'columns': COLUMNS,
+                'description': "Read stats from Bismark BAM.",
+                'bamfn': str(bamfn),
+            }
         )
 
 
@@ -82,8 +143,9 @@ n3	2	allCpG	1	42	10M	*	0	10	NNNNNNNNNN	ABCDEFGHIJ	NM:i:tag0\tMD:Z:tag1\tXM:Z:...
         with open(samfn, 'w') as f:
             f.write(test_sam)
         # run main
-        run(samfn, tmpdir / 'test', True, True)
-        df = pd.read_csv(tmpdir/'test.read-stats.tsv.gz', sep='\t', index_col=0)
+        run(samfn, tmpdir / 'test', True, True, cannon_chrm=False)
+        table, metadat = read_array(tmpdir/'test.read-stats.arr.gz')
+        df = pd.DataFrame(table, columns=COLUMNS)
         print(df)
         # check some numbers in df
         assert df.loc[0, 'FreqC'] == 8
@@ -116,6 +178,10 @@ class ReadStatsArgs:
         help="Output files will begin with this. If output dir doesn't exist, it will be created.",
         aliases=["-o"]
     ))
+    cannonical_chrm: bool = field(metadata=dict(
+        default=False,
+        help='Only include reads from cannonical chromosomes.'
+    ))
     no_plot: bool = field(metadata=dict(
         default=False,
         help='Do not generate a plot of length vs methylation status.'
@@ -128,7 +194,13 @@ class ReadStatsArgs:
 
 def cli():
     args = datargs.parse(ReadStatsArgs)
-    run(args.bamfn, args.out_prefix, not args.no_plot, not args.no_table)
+    run(
+        args.bamfn,
+        args.out_prefix,
+        not args.no_plot,
+        not args.no_table,
+        args.cannonical_chrm
+    )
 
 if __name__ == '__main__':
     print('Running test.')
