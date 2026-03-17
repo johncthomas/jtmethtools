42dc2235e64cf47a47df4f70e42def5330073259
John C. Thomas <jcthomas000@gmail.com>
2026-03-17 15:37:57 +0000

    Remove pairs if either segment has mCH.

--- src/jtmethtools/scripts/filter_CH.py
+++ src/jtmethtools/scripts/filter_CH.py
@@ -1,35 +1,62 @@
 """From a BAM file, write a new bam that does not contain alignments with CH methylation."""
 
 import pysam
-from jtmethtools.alignments import get_bismark_met_str
+from jtmethtools.alignments import get_bismark_met_str, iter_bam_segments
 from datargs import argsclass, arg, parse
 from dataclasses import field
 from pathlib import Path
+from loguru import logger
 
 
+def remove_ch_methylation(bam_file: Path | str, output_file: Path | str,
+                          paired_end: bool, versbose=True):
+    """Write a new BAM file that does not contain alignments with CH methylation.
+    A mCH in either segment of a paired-end read will cause the entire read to be discarded.
+    """
 
-def remove_ch_methylation(bam_file: Path|str, output_file: Path|str, versbose=True):
-    """Remove alignments with CH methylation from a BAM file."""
     ch_meth_symbols = {'X', 'H', 'U'}
-    with pysam.AlignmentFile(bam_file, "rb") as bam_in, \
-         pysam.AlignmentFile(output_file, "wb", template=bam_in) as bam_out:
+
+    with pysam.AlignmentFile(bam_file) as b:
+        header_d = b.header.to_dict()
+        header_d['PG'].append(
+            {'ID': 'jtmethtools CH removal',
+             'CL': f'remove_ch_methylation(bam_file={str(bam_file)}, output_file={str(output_file)}, paired_end={paired_end})'}
+        )
+        header = pysam.AlignmentHeader.from_dict(header_d)
+
+    with pysam.AlignmentFile(output_file, "wb", header=header) as bam_out:
         total_alignments = 0
         written = 0
-        has_ch = 0
+        ch_count = 0
         if versbose:
-            print(f"Writing {output_file}")
-        for alignment in bam_in:
+            print(f"Writing to {output_file}")
+
+        for alns in iter_bam_segments(bam_file, paired_end=paired_end):
             total_alignments += 1
-            # Get the methylation string
-            met_str = get_bismark_met_str(alignment)
-            # Write to out file if no overlapping symbols with CH methylation
-            if set(met_str).isdisjoint(ch_meth_symbols):
+            has_ch = False
+            for alignment in alns:
+                if alignment is None:
+                    continue
+
+                met_str = get_bismark_met_str(alignment)
+
+                # Find symbols in met_str that overlap with bad symbols
+                #   This method chosen after some benchmarking.
+                if not set(met_str).isdisjoint(ch_meth_symbols):
+                    # print(set(met_str))
+                    has_ch = True
+            if has_ch:
+                ch_count += 1
+            if not has_ch:
                 written += 1
-                bam_out.write(alignment)
-            else:
-                has_ch += 1
+                for alignment in alns:
+                    if alignment is None:
+                        continue
+                    bam_out.write(alignment)
+
         if versbose:
-            print(f"{has_ch}/{total_alignments} ({round(has_ch/total_alignments*100, 1):}%) alignments with methylated CH discarded. ")
+            print(
+                f"{ch_count}/{total_alignments} ({round(ch_count / total_alignments * 100, 1):}%) {'paired ' if paired_end else ''}alignments with methylated CH discarded. ")
 
 
 @argsclass(
